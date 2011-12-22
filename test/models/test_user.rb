980ca6fd9def2d9366d928c3728de4583c4cbd02
Oren Golan <orengolan@gmail.com>
2011-12-21 23:09:16 -0800

    minitest for the user model

---
+++ test/models/test_user.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require_relative '../../models/user'
+
+describe User do
+  before do
+    @user = User.new
+  end
+
+  describe "saying hi" do
+    it "respond with hi" do
+      assert_equal "hi", @user.say_hi
+    end
+  end
+end
+
