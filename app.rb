929a64bd30e42081a4c5b45a77c8e09f528e07b2
Oren Golan <orengolan@gmail.com>
2011-12-30 01:44:42 -0800

    better defaults for different environments

--- app.rb
+++ app.rb
@@ -4,16 +4,22 @@ require_relative 'helpers/init'
 require_relative 'models/init'
 
 class MyApp < Sinatra::Base
-  set :root, File.expand_path('../', __FILE__)
+  enable :method_override
+
+  configure do
+    set :app_file, __FILE__
+  end
 
   configure :development do
+    enable :logging, :dump_errors, :raise_errors
   end
 
   configure :qa do
+    enable :logging, :dump_errors, :raise_errors
   end
 
   configure :production do
+    set :raise_errors, false #false will show nicer error page
+    set :show_exceptions, false #true will ignore raise_errors and display backtrace in browser
   end
-
 end
-
